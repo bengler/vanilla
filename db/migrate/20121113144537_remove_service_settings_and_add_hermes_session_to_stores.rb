5718447c9fe6616cb67689a6fc1283fb03367800
Alexander Staubo <alex@bengler.no>
2013-01-24 18:42:56 +0100

    Fix misnamed migration.

--- db/migrate/20121113144537_remove_service_settings_and_add_hermes_session_to_stores.rb
+++ db/migrate/20121113144537_remove_service_settings_and_add_hermes_session_to_stores.rb
@@ -1,4 +1,4 @@
-class RemoveServiceSettingsAndAddHermestSessionToStores < ActiveRecord::Migration
+class RemoveServiceSettingsAndAddHermesSessionToStores < ActiveRecord::Migration
   def self.up
     add_column :stores, :hermes_session, :text
     remove_column :stores, :service_settings
